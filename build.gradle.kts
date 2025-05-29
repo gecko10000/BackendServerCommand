5fd6a45d3bdf210844dff848ec1e7a743dc035cd
gecko10000 <levtey@gmail.com>
2025-05-28 22:33:03 -0500

    Register dummy command at bootstrap for alias compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("de.eldoria.plugin-yml.bukkit") version "0.7.1"
+    id("de.eldoria.plugin-yml.paper") version "0.7.1"
 }
 
 sourceSets {
@@ -16,19 +16,20 @@ sourceSets {
 
 
 group = "gecko10000.backendservercommand"
-val versionString = "0.2"
+val versionString = "0.3"
 version = versionString
 
-bukkit {
+paper {
     name = "BackendServerCommand"
     main = "$group.$name"
     version = versionString
     author = "gecko10000"
-    apiVersion = "1.13"
+    apiVersion = "1.19"
     permissions {
         register("backendservercommand.use")
         register("backendservercommand.use.other")
     }
+    bootstrapper = "$group.CommandBootstrap"
 }
 
 repositories {
---
+++ src/gecko10000/backendservercommand/CommandBootstrap.java
@@ -0,0 +1,15 @@
+package gecko10000.backendservercommand;
+
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+
+public class CommandBootstrap implements PluginBootstrap {
+    @Override
+    public void bootstrap(BootstrapContext context) {
+        context.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            commands.registrar().register(Commands.literal("server").build());
+        });
+    }
+}
